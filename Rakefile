5dccd10c8b46312eb81525d8e444fdd8208f5555
maryfarner18 <maryfarner@gmail.com>
2019-09-27 23:18:27 -0400

    Done.

--- Rakefile
+++ Rakefile
@@ -1,5 +1,7 @@
 require_relative 'config/environment'
 
+require_relative "lib/queries.rb"
+
 desc 'starts a console'
 task :console do
   Pry.start
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,12 @@
 require 'bundler'
+require "sqlite3"
+require 'csv'
+require 'pry'
+
 Bundler.require
 
 # Setup a DB connection here
+DB = SQLite3::Database.new('db/database3.db')
+
+#DB = SQLite3::Database.new(':memory:')
+
